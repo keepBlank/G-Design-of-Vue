492a0c7cb2bd88d0acc9961605a8bbc2ba513b3a
keepBlank <reagen2020whr@gmail.com>
2023-07-01 10:42:22 +0800

    添加icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,30 +1,37 @@
 <template>
-  <button class='g-button'>按钮</button>
+    <button class='g-button' :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <span class="content">
+            <slot></slot>
+        </span>
+    </button>
 </template>
 
 <script>
-export default {}
+export default {
+    props: ['icon', 'icon-position']
+}
 </script>
 
 <style lang="scss">
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  font-size: var(--font-size);height: var(--button-height);
+  padding: 0 .7em;border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);background: var(--button-bg);
+  display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
 
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-
-  &:active {
-    background-color: var(--border-active-bg);
-  }
-
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background-color: var(--border-active-bg);}
   &:focus {
     outline: none;
+  }
+  > .icon{order: 1;margin-right: .1em}
+  > .content{order: 2;}
+  &.icon-right {
+      > .icon{order: 2;margin-right:0;margin-left: .1em}
+      > .content{order: 1;}
   }
 }
 </style>
