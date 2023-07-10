4986a508106360c029f83232b748f2535c2f59a1
keepBlank <front-endligen@foxmail.com>
2023-07-10 10:47:51 +0800

    第一个元素不需要margin-left

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -25,18 +25,13 @@ export default {
 
   > .g-button {
     border-radius: 0;
-    margin-left: -1px;
-
+    &:not(:first-child){margin-left: -1px;}
     &:first-child {
-      border-top-left-radius: var(--border-radius);
-      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: var(--border-radius);border-bottom-left-radius: var(--border-radius);
     }
-
     &:last-child {
-      border-top-right-radius: var(--border-radius);
-      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius);border-bottom-right-radius: var(--border-radius);
     }
-
     &:hover {
       position: relative;
       z-index: 1;
