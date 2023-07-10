13c013449c5e3384d17e0c84f388cf3ad233b5f9
keepBlank <front-endligen@foxmail.com>
2023-07-10 14:29:02 +0800

    使用chai.expect 添加4个测试用例

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import Vue from "vue";
-import Button from "./button.vue";
-import Icon from "./icon.vue";
-import GButtonGroup from "./g-button-group.vue";
+import Vue from 'vue';
+import Button from './button.vue';
+import Icon from './icon.vue';
+import GButtonGroup from './g-button-group.vue';
+import {expect} from 'chai';
 
 // 全局注册组件
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group',GButtonGroup)
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', GButtonGroup);
 
 new Vue({
     el: '#app',
@@ -15,6 +16,81 @@ new Vue({
             loading1: false,
             loading2: false,
             loading3: false
-        }
+        };
     }
-})
+});
+
+// 单元测试icon
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    // mount到内存中
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-setting');
+    vm.$el.remove();
+    vm.$destroy();
+}
+
+// 单元测试loading
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    });
+    // mount到内存中
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading');
+    button.$el.remove();
+    button.$destroy();
+}
+
+// 单元测试svg的样式order
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    // mount到内存中
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let order = window.getComputedStyle(svg).order;
+    expect(order).to.eq('1');
+    vm.$el.remove();
+    vm.$destroy();
+}
+
+// 单元测试iconPosition
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    });
+    // mount到内存中
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let order = window.getComputedStyle(svg).order;
+    expect(order).to.eq('2');
+    vm.$el.remove();
+    vm.$destroy();
+}
+
