ee91f29c9bf2a22be4627e472b845af6149c23b0
keepBlank <front-endligen@foxmail.com>
2023-07-10 10:37:37 +0800

    检测子元素是不是button

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -6,31 +6,43 @@
 
 <script>
 export default {
-  name: "g-button-group"
-}
-</script>
-
-<style lang="scss" scoped>
-  .g-button-group{
-    display: inline-flex;
-    vertical-align: middle;
-    > .g-button{
-      border-radius: 0;
-      margin-left: -1px;
-      &:first-child{
-        border-top-left-radius: var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
-      }
-      &:last-child{
-        border-top-right-radius: var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
-      }
-      &:hover{
-        position: relative;
-        z-index: 1;
+  name: 'g-button-group',
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== 'button') {
+        console.warn(`g-button-group的子元素只能是 g-button,但是你确写的是${name}`);
       }
     }
   }
+};
+</script>
+
+<style lang="scss" scoped>
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .g-button {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
 
 
 </style>
