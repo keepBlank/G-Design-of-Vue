7cea628490527f139ec1808e078373b5e20bf637
keepBlank <front-endligen@foxmail.com>
2023-07-18 13:36:59 +0800

    row 和 col 的雏形完成

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import GIcon from './g-icon.vue';
 import GButtonGroup from './g-button-group.vue';
 import GInput from './g-input.vue';
 import './svg'
-
+import GRow from './g-row.vue';
+import GCol from './g-col.vue';
 
 // 全局注册组件
 Vue.component('g-button', GButton);
 Vue.component('g-icon', GIcon);
 Vue.component('g-button-group', GButtonGroup);
 Vue.component('g-input',GInput)
+Vue.component('g-row',GRow)
+Vue.component('g-col',GCol)
 
 new Vue({
     el: '#app',
---
+++ src/g-col.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'g-col',
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+};
+
+</script>
+
+
+<style scoped lang="scss">
+.col {
+  width: 50%;
+  height: 100px;
+  background: #999;
+  border: 1px solid red;
+
+  $class-prefix: col-;
+  //.col.col-1
+  //.col.col-2
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+
+</style>
