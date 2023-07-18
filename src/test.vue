0108692e8b2fb8b38a1b95b223aec5737c48e5f9
keepBlank <front-endligen@foxmail.com>
2023-07-18 10:33:26 +0800

    add row and col

---
+++ src/test.vue
@@ -0,0 +1,22 @@
+<g-row gutter="12">
+  <g-col span="12"></g-col>
+  <g-col span="12"></g-col>
+</g-row>
+
+<g-row gutter="12">
+<g-col span="8"></g-col>
+<g-col span="8"></g-col>
+<g-col span="8"></g-col>
+</g-row>
+
+<g-row gutter="12">
+<g-col span="6"></g-col>
+<g-col span="6"></g-col>
+<g-col span="6"></g-col>
+<g-col span="6"></g-col>
+</g-row>
+
+<g-row gutter="12">
+<g-col span="2"></g-col>
+<g-col span="22"></g-col>
+</g-row>
