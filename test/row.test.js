fb60ff77702d76ed3e17157bfea94255eb263707
keepBlank <front-endligen@foxmail.com>
2023-07-20 11:02:57 +0800

    添加 g-row 的测试用例

--- test/row.test.js
+++ test/row.test.js
@@ -10,7 +10,7 @@ describe('Row', () => {
     it('存在', () => {
         expect(Row).to.exist;
     });
-    it('接收 gutter 属性', () => {
+    it('接收 gutter 属性', (done) => {
         Vue.component('g-row', Row);
         Vue.component('g-col', Col);
         const div = document.createElement('div');
@@ -24,7 +24,30 @@ describe('Row', () => {
         const vm = new Vue({
             el: div
         })
-        console.log(vm.$el.outerHTML)
+        setTimeout(()=>{
+            const row = vm.$el.querySelector('.row')
+            expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+            expect(getComputedStyle(row).marginRight).to.eq('-10px')
+            const cols = vm.$el.querySelectorAll('.col')
+            expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+            expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+            done()
+            vm.$el.remove()
+            vm.$destroy()
+        }   ,1000)
     });
-
+    it('接收 align 属性',()=>{
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Row)
+        const vm = new Constructor({
+            propsData:{
+                align: 'center'
+            }
+        }).$mount(div)
+        const element = vm.$el
+        expect(getComputedStyle(element).justifyContent).to.equal('center')
+        div.remove()
+        vm.$destroy()
+    })
 });
